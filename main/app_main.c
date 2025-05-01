78dd21480aabc3ec3bd26ab81977aeb0e6db86e9
jeongyeham <jeongyeham@foxmail.com>
2025-05-01 11:50:13 +0800

    refactor(main): comment out mqtt5 and sensor hub tasks
    - Disable mqtt5 and sensor hub tasks by commenting out their creation lines
    - Keep lvgl_ui_task creation line commented out
    
    Signed-off-by: jeongyeham <jeongyeham@foxmail.com>

--- main/app_main.c
+++ main/app_main.c
@@ -18,8 +18,8 @@ _Noreturn void app_main(void) {
     ESP_LOGI(TAG, "Ready to create task(s)");
 
     xTaskCreatePinnedToCore(wifi_task, "wifi_task", 4096, s_sys_event_group, 0, &net_task_handle, PRO_CPU_NUM);
-    xTaskCreatePinnedToCore(mqtt5_start_task, "mqtt5_task", 4096, s_sys_event_group, 0, &mqtt5_handle, APP_CPU_NUM);
-    xTaskCreatePinnedToCore(sensor_hub_task,"sensor_hub_task", 4096,NULL,0,&sensor_hub_handle,APP_CPU_NUM);
+    //xTaskCreatePinnedToCore(mqtt5_start_task, "mqtt5_task", 4096, s_sys_event_group, 0, &mqtt5_handle, APP_CPU_NUM);
+    //xTaskCreatePinnedToCore(sensor_hub_task,"sensor_hub_task", 4096,NULL,0,&sensor_hub_handle,APP_CPU_NUM);
     //xTaskCreatePinnedToCore(lvgl_ui_task,"lvgl_ui_task", 4096,NULL,0,&lvgl_ui_handle,APP_CPU_NUM);
 
     ESP_LOGI(TAG, "Already created %d task(s)", uxTaskGetNumberOfTasks());
