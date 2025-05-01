c5423722acbf6cc7037ab17ad0a812e6d93728a3
jeongyeham <jeongyeham@foxmail.com>
2025-05-01 11:24:44 +0800

    feat(main): add MQTT5, WiFi and smart config functionality
    - Add MQTT5 task for handling MQTT communication
    - Implement WiFi task for managing WiFi connection- Add smart config task for easy WiFi setup
    - Update header file to include new function declarations
    
    Signed-off-by: jeongyeham <jeongyeham@foxmail.com>

--- main/freertos_task.h
+++ main/freertos_task.h
@@ -7,5 +7,8 @@
 
 void sensor_hub_task(void *pvParameters);
 void lvgl_ui_task(void *pvParameters);
+void mqtt5_start_task(void *pvParameters);
+void wifi_task(void *pvParameters);
+void smart_config_task(void *pvParameters);
 
 #endif //FREERTOS_TASK_H
